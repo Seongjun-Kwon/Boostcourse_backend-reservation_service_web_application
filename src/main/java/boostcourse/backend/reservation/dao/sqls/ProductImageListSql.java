e83e5144e3eff13f8d4c20dbbd8e1bfb04be3cf5
Kwon seong jun <tjdwns1865@naver.com>
2022-01-23 04:55:56 +0900

    sql 오류 수정

--- src/main/java/boostcourse/backend/reservation/dao/sqls/ProductImageListSql.java
+++ src/main/java/boostcourse/backend/reservation/dao/sqls/ProductImageListSql.java
@@ -4,7 +4,7 @@ public class ProductImageListSql {
     public static final String SELECT_PRODUCT_IMAGES =
             "SELECT fi.content_type, fi.create_date, fi.delete_flag, pi.file_id, fi.file_name, fi.modify_date, pi.product_id, pi.id, fi.save_file_name, fi.content_type FROM product_image pi "
             + "INNER JOIN file_info fi ON fi.id = pi.file_id "
-            + "WHERE pi.type NOT IN 'th' "
+            + "WHERE pi.type NOT IN ('th') "
             + "AND pi.product_id = :productId "
             + "LIMIT 2 ";
 }
